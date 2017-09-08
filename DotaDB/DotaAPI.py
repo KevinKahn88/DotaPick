2beaba0db32ceaf1f65451ecfb775051f8d7a088
Kevin Kahn <KevinKahn88@gmail.com>
2017-09-08 12:20:15 -0700

    added pickling of the jsondata to avoid calling API constantly to debug

--- DotaDB/DotaAPI.py
+++ DotaDB/DotaAPI.py
@@ -6,10 +6,13 @@ DotaAPI controls calls to Valve's API
 
 from urllib.request import urlopen
 import json
+import pickle
 
 url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001/?start_at_match_seq_num=2900000002&key=DFD1061664AEAC307766E3BD4C824B83'
 
 apiResponse = urlopen(url)
 rawData = apiResponse.read().decode('utf-8')
 jsonData = json.loads(rawData)
+matchData = jsonData['result']['matches']
 
+pickle.dump(matchData,open('matchData.pkl','wb'))
--- DotaDB/DotaDB.py
+++ DotaDB/DotaDB.py
@@ -1,6 +1,32 @@
+
+import pickle
 import sqlalchemy
 import pandas as pd
 
+test_url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001/?start_at_match_seq_num=2900000002&key=DFD1061664AEAC307766E3BD4C824B83'
+
+player_schema = [
+	'account_id',
+	'player_slot',
+	'item_0',
+	'item_1',
+	'item_2',
+	'item_3',
+	'item_4',
+	'item_5',
+	'kills',
+	'deaths',
+	'assists',
+	'leaver_status',
+	'gold',
+	'last_hits',
+	'denies',
+	'xp_per_min',
+	'gold_spent',
+	'hero_damage',
+	'tower_damage',
+	'hero_healing',
+	'level']
 '''
 Establishes connection to psql database
 '''
@@ -15,48 +41,31 @@ Separate out players info into distinct columns
 Applied to players column in df
 '''
 def parse_players_info(players_list):
+	global player_schema
+
 	parsedCols = []
 	for player in enumerate(players_list):
-		parsedCols.append(player['account_id'])
-		parsedCols.append(player['player_slot'])
-		parsedCols.append(player['hero_id'])
-		parsedCols.append(player['item_0'])
-		parsedCols.append(player['item_1'])
-		parsedCols.append(player['item_2'])
-		parsedCols.append(player['item_3'])
-		parsedCols.append(player['item_4'])
-		parsedCols.append(player['item_5'])
-		parsedCols.append(player['kills'])
-		parsedCols.append(player['deaths'])
-		parsedCols.append(player['assists'])
-		parsedCols.append(player['leaver_status'])
-		parsedCols.append(player['gold'])
-		parsedCols.append(player['last_hits'])
-		parsedCols.append(player['denies'])
-		parsedCols.append(player['gold_per_min'])
-		parsedCols.append(player['xp_per_min'])
-		parsedCols.append(player['gold_spent'])
-		parsedCols.append(player['hero_damage'])
-		parsedCols.append(player['tower_damage'])
-		parsedCols.append(player['hero_healing'])
-		parsedCols.append(player['level'])
-
+		parsedCols.append([player[x] for x in player_schema])
 '''
 Converts json matches to dataframe that can export into psql
 Input: array of dictionaries (should be jsonData['result']['matches'])
 Output: dataframe
 '''
 def json_to_df(matchesJson):
-	matchDF = pd.DataFrame(matchesJson)
+	global player_schema
+	team_schema = []
+	for ind in range(10):
+		temp_schema = ['p' + str(ind) + '_' + item for item in player_schema]
+		team_schema.append(temp_schema)
 
+
+	matchDF = pd.DataFrame(matchesJson)
 	matchDF = matchDF[matchDF['human_players']==10]
 
-	# remove columns to be restructured (with nested info)
-	del matchDF['players']
-	if 'picks_bans' in matchDF.keys():
-		del matchDF['picks_bans']
+	matchDF[team_schema] = matchDF['players'].apply(parse_players_info)
+	return matchDF
 
-
-
-
-
+matchData = pickle.load(open('matchData.pkl','rb'))
+matchDF = pd.DataFrame(matchData)
+matchDF = matchDF[matchDF['human_players']==10]
+players_info = matchDF['players'][0]
