a92075ed4b5d3d25c603fc56d75e77981ea46cc1
Kevin Kahn <KevinKahn88@gmail.com>
2017-09-08 10:20:25 -0700

    Finished json parse function, fixed typo in scheme

--- DotaDB/DotaDB.py
+++ DotaDB/DotaDB.py
@@ -16,7 +16,30 @@ Applied to players column in df
 '''
 def parse_players_info(players_list):
 	parsedCols = []
-	for ind, player in enumerate(players_list):
+	for player in enumerate(players_list):
+		parsedCols.append(player['account_id'])
+		parsedCols.append(player['player_slot'])
+		parsedCols.append(player['hero_id'])
+		parsedCols.append(player['item_0'])
+		parsedCols.append(player['item_1'])
+		parsedCols.append(player['item_2'])
+		parsedCols.append(player['item_3'])
+		parsedCols.append(player['item_4'])
+		parsedCols.append(player['item_5'])
+		parsedCols.append(player['kills'])
+		parsedCols.append(player['deaths'])
+		parsedCols.append(player['assists'])
+		parsedCols.append(player['leaver_status'])
+		parsedCols.append(player['gold'])
+		parsedCols.append(player['last_hits'])
+		parsedCols.append(player['denies'])
+		parsedCols.append(player['gold_per_min'])
+		parsedCols.append(player['xp_per_min'])
+		parsedCols.append(player['gold_spent'])
+		parsedCols.append(player['hero_damage'])
+		parsedCols.append(player['tower_damage'])
+		parsedCols.append(player['hero_healing'])
+		parsedCols.append(player['level'])
 
 '''
 Converts json matches to dataframe that can export into psql
@@ -25,6 +48,8 @@ Output: dataframe
 '''
 def json_to_df(matchesJson):
 	matchDF = pd.DataFrame(matchesJson)
+
+	matchDF = matchDF[matchDF['human_players']==10]
 
 	# remove columns to be restructured (with nested info)
 	del matchDF['players']
